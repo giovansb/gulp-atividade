304665946abad5668fcb4bd8c55214994ea7d5d4
Giovana Barbosa <giovana.a.s.barbosa@outlook.com>
2024-07-15 01:03:08 -0300

    first commit

---
+++ source/scripts/main.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById('botao-prod');
+
+botao.addEventListener("click", function () {
+    swal("Obrigada pela intenção!","Infelizmente estamos com complicações, mas o problema será resolvido rapidamente. Tente novamente mais tarde!","warning"); 
+})
+
+
+
+
